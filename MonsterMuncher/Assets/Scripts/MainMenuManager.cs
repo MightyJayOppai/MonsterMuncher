845adcee39d89ef752f82139942b79626432280a
Ajay Rawat <a.rawat@saedubai.com>
2019-07-15 21:43:29 +0400

    Created MainMenu System, New Font as well as a Timer for the game

---
+++ MonsterMuncher/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
--- MonsterMuncher/Assets/Scripts/MonsterMovement.cs
+++ MonsterMuncher/Assets/Scripts/MonsterMovement.cs
@@ -8,6 +8,9 @@ public class MonsterMovement : MonoBehaviour
     Rigidbody monsterRb;
     public float movementSpeed;
     public float rotationSpeed;
+    private Vector3 movementInput;
+    private float moveXAxis;
+    private float moveYAxis;
     void Start()
     {
         monsterRb = GetComponent<Rigidbody>();
@@ -16,11 +19,16 @@ public class MonsterMovement : MonoBehaviour
     
     void Update()
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveXAxis = Input.GetAxis ("Horizontal");
+        float moveYAxis = Input.GetAxis ("Vertical");
 
-        monsterRb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * movementSpeed);
-        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
-        transform.Translate(0,0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+        movementInput = (moveXAxis * transform.right + moveYAxis * transform.forward).normalized;
+        
+        if(movementInput != Vector3.zero)
+        {
+            monsterRb.AddForce(movementInput * movementSpeed * Time.deltaTime, ForceMode.Impulse);
+            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+            transform.Translate(0,0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+        }
     }
 }
