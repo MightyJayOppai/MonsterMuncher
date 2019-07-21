5d74740903c24d056baaee40b455001db8b57d11
Ajay Rawat <a.rawat@saedubai.com>
2019-07-21 16:01:32 +0400

    Created a simple map using ProBuilder as well as changed the movement to be continuous as well as edited the camera (needs tweaking)

--- MonsterMuncher/Assets/Scripts/CameraFollow.cs
+++ MonsterMuncher/Assets/Scripts/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform player;
 
-    public float smoothSpeed;
+    public float smoothSpeed = 2f;
     public Vector3 offset;
     void Start()
     {
@@ -19,7 +19,7 @@ public class CameraFollow : MonoBehaviour
         Vector3 desiredPosition = player.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-
+        //transform.rotation = Quaternion.Lerp(player.rotation,);
         transform.LookAt(player);    
     }
 }
--- MonsterMuncher/Assets/Scripts/MonsterMovement.cs
+++ MonsterMuncher/Assets/Scripts/MonsterMovement.cs
@@ -7,28 +7,56 @@ public class MonsterMovement : MonoBehaviour
     
     Rigidbody monsterRb;
     public float movementSpeed;
-    public float rotationSpeed;
-    private Vector3 movementInput;
-    private float moveXAxis;
-    private float moveYAxis;
+    private Transform monsterRotation;
+    //public float rotationSpeed;
+    //private Vector3 movementInput;
+    //private float moveXAxis;
+    //private float moveYAxis;
+    
+    void Awake()
+    {
+        monsterRotation = GetComponent<Transform>();
+        monsterRb = GetComponent<Rigidbody>();
+    }
     void Start()
     {
-        monsterRb = GetComponent<Rigidbody>();
+
     }
 
     
+    void FixedUpdate()
+    {
+        //Put this in a void Function
+        monsterRb.MovePosition(transform.position + transform.forward * movementSpeed * Time.deltaTime);
+    }
     void Update()
     {
-        float moveXAxis = Input.GetAxis ("Horizontal");
-        float moveYAxis = Input.GetAxis ("Vertical");
-
-        movementInput = (moveXAxis * transform.right + moveYAxis * transform.forward).normalized;
-        
-        if(movementInput != Vector3.zero)
+        //Put this in a void Function
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            monsterRb.AddForce(movementInput * movementSpeed * Time.deltaTime, ForceMode.Impulse);
-            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
-            transform.Translate(0,0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+            monsterRotation.rotation = Quaternion.Euler(0f,90f,0f);
         }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            monsterRotation.rotation = Quaternion.Euler(0f,-90f,0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            monsterRotation.rotation = Quaternion.Euler(0f,180f,0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            monsterRotation.rotation = Quaternion.Euler(0f,0f,0f);
+        }
+        //float moveXAxis = Input.GetAxis ("Horizontal");
+        //float moveYAxis = Input.GetAxis ("Vertical");
+
+        //movementInput = (moveXAxis * transform.right + moveYAxis * transform.forward).normalized;
+        
+        //if(movementInput != Vector3.zero)
+        //{
+        //    monsterRb.AddForce(movementInput * movementSpeed * Time.deltaTime, ForceMode.Impulse);
+        //    transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+        //    transform.Translate(0,0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
     }
 }
